e1bdf54f23bc864164557ee09393780dc80629f8
Vishal Vipinmohan <vishalvipin123@gmail.com>
2021-07-24 19:19:38 +0530

    Hosting
    Adding Firebase

--- officialproject/index.php
+++ officialproject/index.php
@@ -16,6 +16,15 @@
     
 </head>
 <body>
+    <!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="/__/firebase/8.8.0/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="/__/firebase/8.8.0/firebase-analytics.js"></script>
+
+<!-- Initialize Firebase -->
+<script src="/__/firebase/init.js"></script>
 <?php
   $adminuser='admin';
   $adminpwd='admin';
